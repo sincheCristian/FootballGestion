1a14d5120e5fbb5694409179f4db11fe7a53b7a0
sinche <sinchechristian1977@gmail.com>
2026-02-02 16:19:22 +0300

    reset deploy

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app/App";
 import "./styles/index.css";
-import { MatchProvider } from "../src/app/components/context/matchContext";
+import { MatchProvider } from "../src/app/components/context/MatchContext";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
