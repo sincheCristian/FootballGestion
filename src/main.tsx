b4d2060f0d863f9127bdc9e2649bb7ede5af3621
sinche <sinchechristian1977@gmail.com>
2026-02-02 16:40:33 +0300

    fix: ajout du dossier components manquant dans l'import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app/App";
 import "./styles/index.css";
-import { MatchProvider } from "./app/context/MatchContext";
+import { MatchProvider } from "./app/components/context/MatchContext";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
