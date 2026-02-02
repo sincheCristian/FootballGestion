cf52096708e45a9f1152c0f72fadaf1df6cce048
sinche <sinchechristian1977@gmail.com>
2026-02-02 16:27:29 +0300

    modify main

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app/App";
 import "./styles/index.css";
-import { MatchProvider } from "../src/app/components/context/MatchContext";
+import { MatchProvider } from "./app/context/MatchContext";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
